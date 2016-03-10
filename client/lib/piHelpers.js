5686be718a14fc1050b108db75ae0eeeb9d53248
klhutchins1 <klhutchins@gmail.com>
2016-03-10 11:16:24 -0700

    try

--- client/lib/piHelpers.js
+++ client/lib/piHelpers.js
@@ -1,6 +1,5 @@
 Template.toggleLED.events( {
 	"click #ledButton": function () {
-    led.writeSync(1);
     Meteor.call("toggleLED", this._id, ! this.isChecked );
 	}
 
---
+++ server/piMethods.js
@@ -0,0 +1,66 @@
+//adds GPIO for the raspi
+Meteor.methods({
+  toggleLED: function (){
+    console.log('LED should change')
+    if (Meteor.npmRequire('os').arch() === 'arm'){
+      console.log('ARM will allow it to change');
+
+
+      var GPIO = Meteor.npmRequire('onoff').Gpio,
+        led = new GPIO(17, 'out'),
+        iv;
+
+
+
+        // Toggle the state of the LED on GPIO #14 every 200ms.
+        // Here synchronous methods are used. Asynchronous methods are also available.
+        iv = setInterval(function () {
+          led.writeSync(led.readSync() ^ 1); // 1 = on, 0 = off :)
+          console.log('LED should change')
+        }, 200);
+
+        // Stop blinking the LED and turn it off after 5 seconds.
+        setTimeout(function () {
+          clearInterval(iv); // Stop blinking
+          led.writeSync(0);  // Turn LED off.
+          led.unexport();    // Unexport GPIO and free resources
+          console.log('LED should change')
+        }, 5000);
+      //var GPIO = Meteor.npmRequire('onoff').Gpio,
+        //led = new GPIO(17, 'out'),
+        //led.writeSync(led.readSync() ^ 1); // 1 = on, 0 = off :)
+
+
+    }else{
+      console.log('Windows does not have epoll so, LED cant change');
+    }
+
+  }
+});
+
+
+//Wont build unless system is running ARM CPU for rasPi
+//if (Meteor.npmRequire('os').arch() === 'arm'){
+//  console.log('This will only show up when Running on ARM');
+
+    //button = new GPIO(18, 'in', 'both'),
+    //thermometer = new GPIO(7,'in');
+
+
+  //function light(err, state) {
+  //  if(state==1){
+//      led.writeSync(1);
+//      console.log('LED should change')
+//    } else{
+//      led.writeSync(0);
+//      console.log('LED should change')
+//    }
+//  }
+
+//button.watch(light);
+
+
+
+//}else{
+//  console.log('This will only show up when Running on WINDOWS or Non-ARM');
+//}
