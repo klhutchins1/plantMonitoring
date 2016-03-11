e0fd32ca01adb192f02d1ff349e2a36a0a0e7e21
klhutchins1 <klhutchins@gmail.com>
2016-03-11 12:17:45 -0700

    Change LED to turn on and off based on button push
    removed the timed blink interval

--- server/piMethods.js
+++ server/piMethods.js
@@ -3,29 +3,30 @@ Meteor.methods({
   toggleLED: function (){
     console.log('LED should change')
     if (Meteor.npmRequire('os').arch() === 'arm'){
-      console.log('ARM will allow it to change');
-
-
-      var GPIO = Meteor.npmRequire('onoff').Gpio,
+        var GPIO = Meteor.npmRequire('onoff').Gpio,
         led = new GPIO(17, 'out'),
-        iv;
 
+        if(led.readSync() ^ 1){
+          led.writeSync(1);
+          console.log('LED ON')
+        } else{
+          led.writeSync(0);
+          console.log('LED OFF')
+        }
 
 
         // Toggle the state of the LED on GPIO #14 every 200ms.
         // Here synchronous methods are used. Asynchronous methods are also available.
-        iv = setInterval(function () {
-          led.writeSync(led.readSync() ^ 1); // 1 = on, 0 = off :)
-          console.log('LED should change')
-        }, 200);
+      //  iv = setInterval(function () {
+      //    led.writeSync(led.readSync() ^ 1); // 1 = on, 0 = off :)
+      //  }, 200);
 
         // Stop blinking the LED and turn it off after 5 seconds.
-        setTimeout(function () {
-          clearInterval(iv); // Stop blinking
-          led.writeSync(0);  // Turn LED off.
-          led.unexport();    // Unexport GPIO and free resources
-          console.log('LED should change')
-        }, 5000);
+      //  setTimeout(function () {
+      //    clearInterval(iv); // Stop blinking
+      //    led.writeSync(0);  // Turn LED off.
+      //    led.unexport();    // Unexport GPIO and free resources
+      //  }, 5000);
       //var GPIO = Meteor.npmRequire('onoff').Gpio,
         //led = new GPIO(17, 'out'),
         //led.writeSync(led.readSync() ^ 1); // 1 = on, 0 = off :)
